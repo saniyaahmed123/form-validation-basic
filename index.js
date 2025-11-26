2ef7ef8dd7e2f428ae6310ea296466bc9e20adf7
SONIA <soniafirdous1985@gmail.com>
2025-11-26 23:42:06 +0500

    first commit

--- index.js
+++ index.js
@@ -1,54 +1,41 @@
+ // Your Firebase Config
+  const firebaseConfig = {
+    apiKey: "AIzaSyAYi1T-1h4e5i7Xpall_I08DSc5YlyZkuI",
+    authDomain: "fir-form-26dc5.firebaseapp.com",
+    projectId: "fir-form-26dc5",
+    storageBucket: "fir-form-26dc5.firebasestorage.app",
+    messagingSenderId: "695171397080",
+    appId: "1:695171397080:web:dc44940abe9a12dde16ead",
+    measurementId: "G-ZHTPB3ZNQ1"
+  };
 
-var  btn = document.getElementById('btn');
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
-var userName = nameInput.value;
-var passwrd = passswordInput.value;
-var password = passwrd.toLowerCase();
-var symbol = ["@", "#", "$", "%", "!", "*", "?", "_", "-"];
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
 
+  // Save Data Function
+  function saveData() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
 
-if (userName === "" || userName === " ") {
-    alert('please fill the required fields')
-}
-else if (password.length < 8) {
-    alert('password should be atleast 8 characters long')
-}
-var hasSymbol= false;
-for (var i = 0; i < password.length; i++) {
-    for (var j = 0; j < symbol.length; j++) {
-        if (symbol[j] === password[i]) {
-            hasSymbol= true;
-             break
-        }
-        
-        
+    if (name && email) {
+      db.collection("students").add({
+        name: name,
+        email: email,
+        timestamp: new Date()
+      })
+      .then(() => {
+        alert("Data saved successfully!");
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
+    } else {
+      alert("Please fill in both fields!");
     }
-}
-if(hasSymbol ===false){
-     alert(`please enter aleast 1 symbol`)
-}
-var hasLetters= false;
-for (var i = 0; i < password.length; i++) {
-    for (var j = 0; j < letters.length; j++) {
-        if (letters[j] === password[i]) {
-            hasLetters = true;
-
-             break
-        }
-       
-
-    }
-}
-if(hasLetters ===false){
-     alert(`please enter aleast 1 letter`)
-}
-btn.addEventListener('click', function submit (){
-    var nameInput = document.getElementById('name');
-    var passswordInput = document.getElementById('password');
-    alert(
-       ` your form has been successfully submitted`
-    
-    )
-})
+  }
